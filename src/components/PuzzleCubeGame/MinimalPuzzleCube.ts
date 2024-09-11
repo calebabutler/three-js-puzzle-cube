44ce56579ad797ac621d0a5f3c7b21bc7ee5bd38
Caleb Butler <butlercq@gmail.com>
2024-09-10 21:32:32 -0400

    Develop MinimalPuzzleCube, add brute force solver

--- src/components/PuzzleCubeGame/MinimalPuzzleCube.ts
+++ src/components/PuzzleCubeGame/MinimalPuzzleCube.ts
@@ -1,4 +1,7 @@
-const SOLVED_CUBE = "WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBYYYYYYYYY";
+export type MinimalPuzzleCube = string;
+
+export const SOLVED_CUBE: MinimalPuzzleCube =
+    "WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBYYYYYYYYY";
 
 interface PrimitiveMove {
     source: string;
@@ -34,10 +37,100 @@ const MOVE_TYPES: { [key: string]: string | PrimitiveMove } = {
     E: "x S x'",
 };
 
-export default class MinimalPuzzleCube {
-    private cubeState: string;
-
-    constructor() {
-        this.cubeState = SOLVED_CUBE;
+const executeMove = (
+    cube: MinimalPuzzleCube,
+    move: string,
+): MinimalPuzzleCube => {
+    if (move.length > 1) {
+        if (move[1] === "'") {
+            return executeAlgorithm(cube, `${move[0]} ${move[0]} ${move[0]}`);
+        } else if (move[1] === "2") {
+            return executeAlgorithm(cube, `${move[0]} ${move[0]}`);
+        } else {
+            throw new Error("Invalid move type!");
+        }
     }
-}
+
+    const moveData = MOVE_TYPES[move];
+
+    if (typeof moveData === "string") {
+        return executeAlgorithm(cube, moveData);
+    } else {
+        const map: { [key: string]: string } = {};
+        for (let i = 0; i < cube.length; i++) {
+            map[moveData.source[i]] = cube[i];
+        }
+        let newCube: string = "";
+        for (let i = 0; i < cube.length; i++) {
+            if (moveData.target[i] === "=") {
+                newCube += cube[i];
+            } else {
+                newCube += map[moveData.target[i]];
+            }
+        }
+        return newCube;
+    }
+};
+
+export const executeAlgorithm = (
+    cube: MinimalPuzzleCube,
+    algorithm: string,
+): MinimalPuzzleCube => {
+    try {
+        if (algorithm === "") {
+            return cube;
+        }
+
+        const moveList = algorithm.trim().split(/\s+/);
+        let newCube: MinimalPuzzleCube = cube;
+        for (const move of moveList) {
+            newCube = executeMove(newCube, move);
+        }
+        return newCube;
+    } catch (error) {
+        console.log(`Algorithm: ${algorithm}`);
+        throw error;
+    }
+};
+
+const numberToAlgorithm = (n: number): string => {
+    const MOVE_ARRAY = [
+        "",
+        "U",
+        "U'",
+        "U2",
+        "D",
+        "D'",
+        "D2",
+        "F",
+        "F'",
+        "F2",
+        "B",
+        "B'",
+        "B2",
+        "L",
+        "L'",
+        "L2",
+        "R",
+        "R'",
+        "R2",
+    ];
+    const numberInMoveArrayRadix = n.toString(MOVE_ARRAY.length);
+    const algorithm = [];
+    for (const digit of numberInMoveArrayRadix) {
+        const index = parseInt(digit, MOVE_ARRAY.length);
+        algorithm.push(MOVE_ARRAY[index]);
+    }
+    return algorithm.join(" ");
+};
+
+export const bruteForceSolve = (cube: MinimalPuzzleCube): string => {
+    let iterator: number = 0;
+    let algorithm: string = "";
+
+    do {
+        algorithm = numberToAlgorithm(iterator);
+        iterator++;
+    } while (executeAlgorithm(cube, algorithm) !== SOLVED_CUBE);
+    return algorithm;
+};
