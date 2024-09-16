a68c529884dab98fe4c96f0f4b8b7bb207644e59
Caleb Butler <butlercq@gmail.com>
2024-09-15 22:25:08 -0400

    Replace decomposition with faster swap

--- src/components/PuzzleCubeGame/MinimalPuzzleCube.ts
+++ src/components/PuzzleCubeGame/MinimalPuzzleCube.ts
@@ -25,10 +25,37 @@ type Edges = [
 
 type Corners = [Corner, Corner, Corner, Corner, Corner, Corner, Corner, Corner];
 
+const swapTwo = (array: any[], index1: number, index2: number): void => {
+    const tmp = array[index2];
+    array[index2] = array[index1];
+    array[index1] = tmp;
+};
+
+const swapFour = (
+    array: any[],
+    resultIndex1: number,
+    resultIndex2: number,
+    resultIndex3: number,
+    resultIndex4: number,
+    index1: number,
+    index2: number,
+    index3: number,
+    index4: number,
+): void => {
+    const tmp1 = array[index1];
+    const tmp2 = array[index2];
+    const tmp3 = array[index3];
+    const tmp4 = array[index4];
+    array[resultIndex1] = tmp1;
+    array[resultIndex2] = tmp2;
+    array[resultIndex3] = tmp3;
+    array[resultIndex4] = tmp4;
+};
+
 const shuffle = (array: any[]): void => {
     for (let i = array.length - 1; i > 1; i--) {
         const j = Math.floor(Math.random() * i);
-        [array[j], array[i]] = [array[i], array[j]];
+        swapTwo(array, i, j);
     }
 };
 
@@ -256,140 +283,32 @@ export class MinimalPuzzleCube {
     private executeMove(move: string): void {
         switch (move) {
             case "U":
-                [this.edges[1], this.edges[2], this.edges[3], this.edges[0]] = [
-                    this.edges[0],
-                    this.edges[1],
-                    this.edges[2],
-                    this.edges[3],
-                ];
-                [
-                    this.corners[1],
-                    this.corners[2],
-                    this.corners[3],
-                    this.corners[0],
-                ] = [
-                    this.corners[0],
-                    this.corners[1],
-                    this.corners[2],
-                    this.corners[3],
-                ];
+                swapFour(this.edges, 1, 2, 3, 0, 0, 1, 2, 3);
+                swapFour(this.corners, 1, 2, 3, 0, 0, 1, 2, 3);
                 break;
             case "U'":
-                [this.edges[3], this.edges[0], this.edges[1], this.edges[2]] = [
-                    this.edges[0],
-                    this.edges[1],
-                    this.edges[2],
-                    this.edges[3],
-                ];
-                [
-                    this.corners[3],
-                    this.corners[0],
-                    this.corners[1],
-                    this.corners[2],
-                ] = [
-                    this.corners[0],
-                    this.corners[1],
-                    this.corners[2],
-                    this.corners[3],
-                ];
+                swapFour(this.edges, 3, 0, 1, 2, 0, 1, 2, 3);
+                swapFour(this.corners, 3, 0, 1, 2, 0, 1, 2, 3);
                 break;
             case "U2":
-                [this.edges[2], this.edges[3], this.edges[0], this.edges[1]] = [
-                    this.edges[0],
-                    this.edges[1],
-                    this.edges[2],
-                    this.edges[3],
-                ];
-                [
-                    this.corners[2],
-                    this.corners[3],
-                    this.corners[0],
-                    this.corners[1],
-                ] = [
-                    this.corners[0],
-                    this.corners[1],
-                    this.corners[2],
-                    this.corners[3],
-                ];
+                swapFour(this.edges, 2, 3, 0, 1, 0, 1, 2, 3);
+                swapFour(this.corners, 2, 3, 0, 1, 0, 1, 2, 3);
                 break;
             case "D":
-                [this.edges[9], this.edges[10], this.edges[11], this.edges[8]] =
-                    [
-                        this.edges[8],
-                        this.edges[9],
-                        this.edges[10],
-                        this.edges[11],
-                    ];
-                [
-                    this.corners[5],
-                    this.corners[6],
-                    this.corners[7],
-                    this.corners[4],
-                ] = [
-                    this.corners[4],
-                    this.corners[5],
-                    this.corners[6],
-                    this.corners[7],
-                ];
+                swapFour(this.edges, 9, 10, 11, 8, 8, 9, 10, 11);
+                swapFour(this.corners, 5, 6, 7, 4, 4, 5, 6, 7);
                 break;
             case "D'":
-                [this.edges[11], this.edges[8], this.edges[9], this.edges[10]] =
-                    [
-                        this.edges[8],
-                        this.edges[9],
-                        this.edges[10],
-                        this.edges[11],
-                    ];
-                [
-                    this.corners[7],
-                    this.corners[4],
-                    this.corners[5],
-                    this.corners[6],
-                ] = [
-                    this.corners[4],
-                    this.corners[5],
-                    this.corners[6],
-                    this.corners[7],
-                ];
+                swapFour(this.edges, 11, 8, 9, 10, 8, 9, 10, 11);
+                swapFour(this.corners, 7, 4, 5, 6, 4, 5, 6, 7);
                 break;
             case "D2":
-                [this.edges[10], this.edges[11], this.edges[8], this.edges[9]] =
-                    [
-                        this.edges[8],
-                        this.edges[9],
-                        this.edges[10],
-                        this.edges[11],
-                    ];
-                [
-                    this.corners[6],
-                    this.corners[7],
-                    this.corners[4],
-                    this.corners[5],
-                ] = [
-                    this.corners[4],
-                    this.corners[5],
-                    this.corners[6],
-                    this.corners[7],
-                ];
+                swapFour(this.edges, 10, 11, 8, 9, 8, 9, 10, 11);
+                swapFour(this.corners, 6, 7, 4, 5, 4, 5, 6, 7);
                 break;
             case "R":
-                [this.edges[7], this.edges[1], this.edges[9], this.edges[6]] = [
-                    this.edges[1],
-                    this.edges[6],
-                    this.edges[7],
-                    this.edges[9],
-                ];
-                [
-                    this.corners[6],
-                    this.corners[1],
-                    this.corners[2],
-                    this.corners[5],
-                ] = [
-                    this.corners[1],
-                    this.corners[2],
-                    this.corners[5],
-                    this.corners[6],
-                ];
+                swapFour(this.edges, 7, 1, 9, 6, 1, 6, 7, 9);
+                swapFour(this.corners, 6, 1, 2, 5, 1, 2, 5, 6);
                 this.corners[1].orientation =
                     (this.corners[1].orientation + 2) % 3;
                 this.corners[2].orientation =
@@ -400,23 +319,8 @@ export class MinimalPuzzleCube {
                     (this.corners[6].orientation + 1) % 3;
                 break;
             case "R'":
-                [this.edges[6], this.edges[9], this.edges[1], this.edges[7]] = [
-                    this.edges[1],
-                    this.edges[6],
-                    this.edges[7],
-                    this.edges[9],
-                ];
-                [
-                    this.corners[2],
-                    this.corners[5],
-                    this.corners[6],
-                    this.corners[1],
-                ] = [
-                    this.corners[1],
-                    this.corners[2],
-                    this.corners[5],
-                    this.corners[6],
-                ];
+                swapFour(this.edges, 6, 9, 1, 7, 1, 6, 7, 9);
+                swapFour(this.corners, 2, 5, 6, 1, 1, 2, 5, 6);
                 this.corners[1].orientation =
                     (this.corners[1].orientation + 2) % 3;
                 this.corners[2].orientation =
@@ -427,43 +331,12 @@ export class MinimalPuzzleCube {
                     (this.corners[6].orientation + 1) % 3;
                 break;
             case "R2":
-                [this.edges[9], this.edges[7], this.edges[6], this.edges[1]] = [
-                    this.edges[1],
-                    this.edges[6],
-                    this.edges[7],
-                    this.edges[9],
-                ];
-                [
-                    this.corners[5],
-                    this.corners[6],
-                    this.corners[1],
-                    this.corners[2],
-                ] = [
-                    this.corners[1],
-                    this.corners[2],
-                    this.corners[5],
-                    this.corners[6],
-                ];
+                swapFour(this.edges, 9, 7, 6, 1, 1, 6, 7, 9);
+                swapFour(this.corners, 5, 6, 1, 2, 1, 2, 5, 6);
                 break;
             case "L":
-                [this.edges[5], this.edges[3], this.edges[11], this.edges[4]] =
-                    [
-                        this.edges[3],
-                        this.edges[4],
-                        this.edges[5],
-                        this.edges[11],
-                    ];
-                [
-                    this.corners[3],
-                    this.corners[4],
-                    this.corners[7],
-                    this.corners[0],
-                ] = [
-                    this.corners[0],
-                    this.corners[3],
-                    this.corners[4],
-                    this.corners[7],
-                ];
+                swapFour(this.edges, 5, 3, 11, 4, 3, 4, 5, 11);
+                swapFour(this.corners, 3, 4, 7, 0, 0, 3, 4, 7);
                 this.corners[0].orientation =
                     (this.corners[0].orientation + 1) % 3;
                 this.corners[3].orientation =
@@ -474,24 +347,8 @@ export class MinimalPuzzleCube {
                     (this.corners[7].orientation + 2) % 3;
                 break;
             case "L'":
-                [this.edges[4], this.edges[11], this.edges[3], this.edges[5]] =
-                    [
-                        this.edges[3],
-                        this.edges[4],
-                        this.edges[5],
-                        this.edges[11],
-                    ];
-                [
-                    this.corners[7],
-                    this.corners[0],
-                    this.corners[3],
-                    this.corners[4],
-                ] = [
-                    this.corners[0],
-                    this.corners[3],
-                    this.corners[4],
-                    this.corners[7],
-                ];
+                swapFour(this.edges, 4, 11, 3, 5, 3, 4, 5, 11);
+                swapFour(this.corners, 7, 0, 3, 4, 0, 3, 4, 7);
                 this.corners[0].orientation =
                     (this.corners[0].orientation + 1) % 3;
                 this.corners[3].orientation =
@@ -502,43 +359,12 @@ export class MinimalPuzzleCube {
                     (this.corners[7].orientation + 2) % 3;
                 break;
             case "L2":
-                [this.edges[11], this.edges[5], this.edges[4], this.edges[3]] =
-                    [
-                        this.edges[3],
-                        this.edges[4],
-                        this.edges[5],
-                        this.edges[11],
-                    ];
-                [
-                    this.corners[4],
-                    this.corners[7],
-                    this.corners[0],
-                    this.corners[3],
-                ] = [
-                    this.corners[0],
-                    this.corners[3],
-                    this.corners[4],
-                    this.corners[7],
-                ];
+                swapFour(this.edges, 11, 5, 4, 3, 3, 4, 5, 11);
+                swapFour(this.corners, 4, 7, 0, 3, 0, 3, 4, 7);
                 break;
             case "F":
-                [this.edges[6], this.edges[2], this.edges[8], this.edges[5]] = [
-                    this.edges[2],
-                    this.edges[5],
-                    this.edges[6],
-                    this.edges[8],
-                ];
-                [
-                    this.corners[5],
-                    this.corners[2],
-                    this.corners[3],
-                    this.corners[4],
-                ] = [
-                    this.corners[2],
-                    this.corners[3],
-                    this.corners[4],
-                    this.corners[5],
-                ];
+                swapFour(this.edges, 6, 2, 8, 5, 2, 5, 6, 8);
+                swapFour(this.corners, 5, 2, 3, 4, 2, 3, 4, 5);
                 this.edges[2].orientation = (this.edges[2].orientation + 1) % 2;
                 this.edges[5].orientation = (this.edges[5].orientation + 1) % 2;
                 this.edges[6].orientation = (this.edges[6].orientation + 1) % 2;
@@ -553,23 +379,8 @@ export class MinimalPuzzleCube {
                     (this.corners[5].orientation + 1) % 3;
                 break;
             case "F'":
-                [this.edges[5], this.edges[8], this.edges[2], this.edges[6]] = [
-                    this.edges[2],
-                    this.edges[5],
-                    this.edges[6],
-                    this.edges[8],
-                ];
-                [
-                    this.corners[3],
-                    this.corners[4],
-                    this.corners[5],
-                    this.corners[2],
-                ] = [
-                    this.corners[2],
-                    this.corners[3],
-                    this.corners[4],
-                    this.corners[5],
-                ];
+                swapFour(this.edges, 5, 8, 2, 6, 2, 5, 6, 8);
+                swapFour(this.corners, 3, 4, 5, 2, 2, 3, 4, 5);
                 this.edges[2].orientation = (this.edges[2].orientation + 1) % 2;
                 this.edges[5].orientation = (this.edges[5].orientation + 1) % 2;
                 this.edges[6].orientation = (this.edges[6].orientation + 1) % 2;
@@ -584,43 +395,12 @@ export class MinimalPuzzleCube {
                     (this.corners[5].orientation + 1) % 3;
                 break;
             case "F2":
-                [this.edges[8], this.edges[6], this.edges[5], this.edges[2]] = [
-                    this.edges[2],
-                    this.edges[5],
-                    this.edges[6],
-                    this.edges[8],
-                ];
-                [
-                    this.corners[4],
-                    this.corners[5],
-                    this.corners[2],
-                    this.corners[3],
-                ] = [
-                    this.corners[2],
-                    this.corners[3],
-                    this.corners[4],
-                    this.corners[5],
-                ];
+                swapFour(this.edges, 8, 6, 5, 2, 2, 5, 6, 8);
+                swapFour(this.corners, 4, 5, 2, 3, 2, 3, 4, 5);
                 break;
             case "B":
-                [this.edges[4], this.edges[10], this.edges[0], this.edges[7]] =
-                    [
-                        this.edges[0],
-                        this.edges[4],
-                        this.edges[7],
-                        this.edges[10],
-                    ];
-                [
-                    this.corners[7],
-                    this.corners[0],
-                    this.corners[1],
-                    this.corners[6],
-                ] = [
-                    this.corners[0],
-                    this.corners[1],
-                    this.corners[6],
-                    this.corners[7],
-                ];
+                swapFour(this.edges, 4, 10, 0, 7, 0, 4, 7, 10);
+                swapFour(this.corners, 7, 0, 1, 6, 0, 1, 6, 7);
                 this.edges[0].orientation = (this.edges[0].orientation + 1) % 2;
                 this.edges[4].orientation = (this.edges[4].orientation + 1) % 2;
                 this.edges[7].orientation = (this.edges[7].orientation + 1) % 2;
@@ -636,24 +416,8 @@ export class MinimalPuzzleCube {
                     (this.corners[7].orientation + 1) % 3;
                 break;
             case "B'":
-                [this.edges[7], this.edges[0], this.edges[10], this.edges[4]] =
-                    [
-                        this.edges[0],
-                        this.edges[4],
-                        this.edges[7],
-                        this.edges[10],
-                    ];
-                [
-                    this.corners[1],
-                    this.corners[6],
-                    this.corners[7],
-                    this.corners[0],
-                ] = [
-                    this.corners[0],
-                    this.corners[1],
-                    this.corners[6],
-                    this.corners[7],
-                ];
+                swapFour(this.edges, 7, 0, 10, 4, 0, 4, 7, 10);
+                swapFour(this.corners, 1, 6, 7, 0, 0, 1, 6, 7);
                 this.edges[0].orientation = (this.edges[0].orientation + 1) % 2;
                 this.edges[4].orientation = (this.edges[4].orientation + 1) % 2;
                 this.edges[7].orientation = (this.edges[7].orientation + 1) % 2;
@@ -669,24 +433,8 @@ export class MinimalPuzzleCube {
                     (this.corners[7].orientation + 1) % 3;
                 break;
             case "B2":
-                [this.edges[10], this.edges[7], this.edges[4], this.edges[0]] =
-                    [
-                        this.edges[0],
-                        this.edges[4],
-                        this.edges[7],
-                        this.edges[10],
-                    ];
-                [
-                    this.corners[6],
-                    this.corners[7],
-                    this.corners[0],
-                    this.corners[1],
-                ] = [
-                    this.corners[0],
-                    this.corners[1],
-                    this.corners[6],
-                    this.corners[7],
-                ];
+                swapFour(this.edges, 10, 7, 4, 0, 0, 4, 7, 10);
+                swapFour(this.corners, 6, 7, 0, 1, 0, 1, 6, 7);
                 break;
             default:
                 throw new Error(`Cannot execute move ${move}`);
